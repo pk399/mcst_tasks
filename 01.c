4147682e29b5e26b6b6d4973eeee078e83c939da
pk399 <63912326+pk399@users.noreply.github.com>
2025-04-08 01:09:29 +0300

    01.c: fix sorting order

--- 01.c
+++ 01.c
@@ -11,7 +11,7 @@ int cmp_int(const void* p, const void* q)
     const int* el1 = p;
     const int* el2 = q;
 
-    return *el1 > *el2;
+    return *el1 == *el2 ? 0 : ( *el1 < *el2 ? -1 : 1 );
 }
 
 int main()
@@ -30,7 +30,7 @@ int main()
 
     for (int i = 1; i < N; i++)
     {
-        if (arr[i - 1] < arr[i]) sorted = false;
+        if (arr[i - 1] > arr[i]) sorted = false;
     }
 
     if (sorted) printf("Sorted!\n");
